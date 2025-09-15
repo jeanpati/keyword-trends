8f847f9b5d2dd52d9a16c3881d3e4d50dd30732a
Jean Patimeteeporn <jpatimet@gmail.com>
2025-09-15 07:11:15 -0500

    added video statistics silver model

---
+++ dbt/keyword_trends/models/silver/silver_video_statistics.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized='table',
+    pre_hook=[
+        "SET s3_endpoint='localhost:9000'",
+        "SET s3_use_ssl=false", 
+        "SET s3_access_key_id='admin'",
+        "SET s3_secret_access_key='password'",
+        "SET s3_url_style='path'"
+    ]
+) }}
+
+
+SELECT 
+    video_id,
+    view_count,
+    like_count,
+    comment_count,
+    duration,
+    (
+        COALESCE(CAST(NULLIF(REGEXP_EXTRACT(duration, 'PT(\d+)H', 1), '') AS INT), 0) * 3600 +
+        COALESCE(CAST(NULLIF(REGEXP_EXTRACT(duration, '(\d+)M', 1), '') AS INT), 0) * 60 +
+        COALESCE(CAST(NULLIF(REGEXP_EXTRACT(duration, '(\d+)S', 1), '') AS INT), 0)
+    ) AS duration_seconds,
+    retrieved_at
+FROM {{ source('bronze', 'video_statistics') }}
