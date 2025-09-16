122d05e6eccf898ebee1d40bebae72f466acfa35
Jean Patimeteeporn <jpatimet@gmail.com>
2025-09-16 05:26:35 -0500

    added dbt gold layer dag

---
+++ airflow/dags/dbt_gold_dag.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+
+with DAG(
+    dag_id="dbt_gold_dag",
+    schedule_interval="@daily",
+    start_date=days_ago(1),
+    catchup=False,
+    description="Run gold models",
+    tags=["dbt", "transformation", "gold", "scheduled"],
+) as dag:
+
+    DBT_PROJECT_DIR = "/opt/airflow/project_root/dbt/keyword_trends"
+    DBT_PROFILES_DIR = "/opt/airflow/project_root/dbt/keyword_trends"
+
+    dbt_debug = BashOperator(
+        task_id="dbt_debug",
+        bash_command=f"cd {DBT_PROJECT_DIR} && dbt debug --profiles-dir {DBT_PROFILES_DIR}",
+        dag=dag,
+    )
+
+    dbt_deps = BashOperator(
+        task_id="dbt_deps",
+        bash_command=f"cd {DBT_PROJECT_DIR} && dbt deps --profiles-dir {DBT_PROFILES_DIR}",
+        dag=dag,
+    )
+
+    dbt_run_gold = BashOperator(
+        task_id="dbt_run_gold",
+        bash_command=f"cd {DBT_PROJECT_DIR} && dbt run --select gold --profiles-dir {DBT_PROFILES_DIR}",
+        dag=dag,
+    )
+
+    dbt_test_staging = BashOperator(
+        task_id="dbt_test_staging",
+        bash_command=f"cd {DBT_PROJECT_DIR} && dbt test --select gold --profiles-dir {DBT_PROFILES_DIR}",
+        dag=dag,
+    )
+
+    dbt_docs_generate = BashOperator(
+        task_id="dbt_docs_generate",
+        bash_command=f"cd {DBT_PROJECT_DIR} && dbt docs generate --profiles-dir {DBT_PROFILES_DIR}",
+        dag=dag,
+    )
+
+    dbt_debug >> dbt_deps >> dbt_run_gold >> dbt_test_staging >> dbt_docs_generate
