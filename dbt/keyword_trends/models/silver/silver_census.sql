aa9ab26da78df0ab4ceb0e52ca7380ef1bf6ff5d
Jean Patimeteeporn <jpatimet@gmail.com>
2025-09-16 05:22:43 -0500

    added dags for dbt silver and update video statistics

--- dbt/keyword_trends/models/silver/silver_census.sql
+++ dbt/keyword_trends/models/silver/silver_census.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized='table',
     pre_hook=[
-        "SET s3_endpoint='localhost:9000'",
+        "SET s3_endpoint='{{ env_var('MINIO_URL', 'localhost:9000') }}'",
         "SET s3_use_ssl=false", 
         "SET s3_access_key_id='admin'",
         "SET s3_secret_access_key='password'",
